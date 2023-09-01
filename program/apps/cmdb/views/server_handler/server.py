ddf0249abdf14271daf10ed727c02a245a84de81
aaron <changde.gan@iluvatar.com>
2023-09-02 00:56:01 +0800

    增加增删改的审计记录

--- program/apps/cmdb/views/server_handler/server.py
+++ program/apps/cmdb/views/server_handler/server.py
@@ -1,6 +1,11 @@
+from fastapi import Request
+
+from apps.admin.models import OperationRecord
 from apps.cmdb.models import Server, ServerType, CreatedBy
 from apps.cmdb.views.server_handler.server_type import ServerQueryData, ServerCreateData, ServerUpdateData
 from oracle.sqlalchemy import SQLAlchemyCRUDRouter, ValidationError
+from watchtower import PayloadData
+from watchtower.settings import logger
 
 
 class ServerCRUDRouter(SQLAlchemyCRUDRouter):
@@ -13,6 +18,81 @@ class ServerCRUDRouter(SQLAlchemyCRUDRouter):
             raise ValidationError(f"添加方式不符合要求: {created_by}")
 
         return await super()._create_validator(item)
+
+    async def _post_create(self, item, request: Request | None = None, payload: PayloadData | None = None):
+        # TODO 未验证转发情况的客户端ip
+        login_ip = request.client.host
+
+        # 记录新建主机，并且记录时出错不影响用户正常添加
+        try:
+            async with self.db_func().begin() as session:
+                record_for_create_server = OperationRecord(
+                    user_id=payload.data.id,
+                    username=payload.data.username,
+                    name=payload.data.name,
+                    login_ip=login_ip,
+                    method=request.method,
+                    uri=request.url.path,
+                    data=f"新建主机记录: {item}"
+                )
+                session.add(record_for_create_server)
+
+                await session.commit()
+                await session.flush()
+        except Exception as error:
+            logger.error(f"记录新建主机记录时出错: {error}")
+
+    async def _post_update(self, item: dict, original_data: dict, request: Request | None = None, payload: PayloadData | None = None) -> dict:
+        # TODO 未验证转发情况的客户端ip
+        login_ip = request.client.host
+
+        data = f"更新前数据: {original_data}, 更新后数据: {item}"
+        # 记录更新主机，并且记录时出错不影响用户正常更新
+        try:
+            async with self.db_func().begin() as session:
+                record_for_update_server = OperationRecord(
+                    user_id=payload.data.id,
+                    username=payload.data.username,
+                    name=payload.data.name,
+                    login_ip=login_ip,
+                    method=request.method,
+                    uri=request.url.path,
+                    data=data
+                )
+                session.add(record_for_update_server)
+
+                await session.commit()
+                await session.flush()
+        except Exception as error:
+            logger.error(f"记录更新主机记录时出错: {error}")
+
+        return item
+
+    async def _post_delete(self, item: dict, request: Request | None = None, payload: PayloadData | None = None) -> dict:
+        # TODO 未验证转发情况的客户端ip
+        login_ip = request.client.host
+
+        data = f"删除数据: {item}"
+        # 记录删除主机，并且记录时出错不影响用户正常删除
+        try:
+            async with self.db_func().begin() as session:
+                record_for_delete_server = OperationRecord(
+                    user_id=payload.data.id,
+                    username=payload.data.username,
+                    name=payload.data.name,
+                    login_ip=login_ip,
+                    method=request.method,
+                    uri=request.url.path,
+                    data=data
+                )
+                session.add(record_for_delete_server)
+
+                await session.commit()
+                await session.flush()
+        except Exception as error:
+            logger.error(f"记录删除主机记录时出错: {error}")
+
+        return item
 
 
 router = ServerCRUDRouter(
