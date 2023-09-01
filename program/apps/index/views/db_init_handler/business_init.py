d8840ad126ff12febf5931c4fb189d9828569cab
aaron <changde.gan@iluvatar.com>
2023-09-01 12:48:20 +0800

    增加业务默认初始化数据

--- program/apps/index/views/db_init_handler/business_init.py
+++ program/apps/index/views/db_init_handler/business_init.py
@@ -1,2 +1,20 @@
+from sqlalchemy import Select
+
+from apps.cmdb.models import ServerTag
+from oracle.sqlalchemy import sql_helper
+
+
 async def run(*args, **kwargs):
-    pass
+    session_maker = sql_helper.get_session()
+
+    async with session_maker.begin() as session:
+        # 确保有一个默认的标签
+        select_default_server_tag_statement = Select(ServerTag).where(ServerTag.id == 1)
+        default_server_tag = (await session.execute(select_default_server_tag_statement)).scalar_one_or_none()
+
+        if not default_server_tag:
+            default_server_tag = ServerTag(name="default", detail="默认标签")
+            session.add(default_server_tag)
+
+        await session.commit()
+        await session.flush()
