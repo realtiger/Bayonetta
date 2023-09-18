f3a8e4f4b790b5b671b33485b8bffe99cfe2e7e3
aaron <changde.gan@iluvatar.com>
2023-09-18 23:24:59 +0800

    删除创建修改时默认为空的字段，否则增加数据时有概率会bug

--- program/apps/admin/views/role_handler/role_type.py
+++ program/apps/admin/views/role_handler/role_type.py
@@ -6,7 +6,7 @@ from oracle.types import QueryBaseModel, UpdateAddFields
 # ### 数据格式定义 ###
 # 单个新建格式
 class RoleCreateData(BaseModel):
-    name: str = Field(default="", description="角色名字", title="角色名字", example="name", min_length=1)
+    name: str = Field(description="角色名字", title="角色名字", example="name", min_length=1)
     detail: str = Field(default="", description="详情", title="详情", example="详情信息")
 
 
